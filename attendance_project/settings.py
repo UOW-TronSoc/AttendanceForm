89c3961ada789fc24d19daf969c858834737ace1
Soham Verma <sohamverma26@gmail.com>
2025-06-03 01:27:15 +1000

    Add data migration to auto-create superuser from env vars

--- attendance_project/settings.py
+++ attendance_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "")
+ADMIN_EMAIL    = os.getenv("ADMIN_EMAIL", "")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "")
